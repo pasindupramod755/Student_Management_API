729d019a0b2af65352f1f2c01f5c9c9f48977b23
pasindupramod755 <pasidubandara12345@gmail.com>
2025-10-08 11:00:31 -0700

    Add Contraller and dto

---
+++ src/main/java/org/example/model/dto/Student.java
@@ -0,0 +1,15 @@
+package org.example.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Student {
+    private String id;
+    private String name;
+    private String address;
+    private int age;
+}
